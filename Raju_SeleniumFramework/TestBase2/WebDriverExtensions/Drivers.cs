04cc75bf22aa64fe07bae6ecf5dd75de1d9260d4
Deiveegarajan Gunasekaran <gdraju20@gmail.com>
2020-02-05 17:47:09 +0530

    "Added Selenium extension and common methods"

---
+++ Raju_SeleniumFramework/TestBase2/WebDriverExtensions/Drivers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestBase2
+{
+    public static class Drivers
+    {
+        public static void RunTest()
+        {
+
+        }
+    }
+}
